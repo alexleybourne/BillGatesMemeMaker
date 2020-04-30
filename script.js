2b15a8571f7dac0ccfb8d3fc59bb960a45192bbd
alexleybourne <alexleybourne1@gmail.com>
2020-05-01 09:26:32 +1000

    Added download template button

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const loaded = () => {
+    console.log('WTF')
+    var ctx = document.getElementById('canvas').getContext('2d'),
+    img = new Image();
+    url = window.URL || window.webkitURL,
+    src = url.createObjectURL(f);
+    img.src = './images/BillGatesMemeTemplate.png';
+    ctx.drawImage(img, 0, 0);    
+};
 
-
-function draw() {
+const draw = () => {
     var ctx = document.getElementById('canvas').getContext('2d'),
         img = new Image(),
         f = document.getElementById("upload-image").files[0],
@@ -13,20 +21,28 @@ function draw() {
       url.revokeObjectURL(src);
     }
     document.getElementById("upload-image").value=""
-}
+};
 
-function downloadCanvas(){
+const  downloadCanvas = () => {
     var canvas = document.getElementById('canvas');
     var image = canvas.toDataURL('image/jpeg', 1.0);
     var link = document.createElement('a');
     link.download = "ImageTest!.jpg";
     link.href = image;
     link.click();
-}
+};
 
-function clearCanvas(){
+const clearCanvas = () => {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
-}
+};
+
+const downloadTemplateImage = () => {
+    var image = './images/BillGatesMemeTemplate.png';
+    var link = document.createElement('a');
+    link.download = "BillGatesMemeTemplate.jpg";
+    link.href = image;
+    link.click();
+};
 
