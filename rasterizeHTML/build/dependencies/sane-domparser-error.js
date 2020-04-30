4cde02f7c2eb7e4e9eeda40e585dc9c69bfb510b
alexleybourne <alexleybourne1@gmail.com>
2020-04-30 12:56:17 +1000

    rasterize HTML installed lets go

---
+++ rasterizeHTML/build/dependencies/sane-domparser-error.js
@@ -0,0 +1,82 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.sanedomparsererror = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+'use strict';
+
+var innerXML = function (node) {
+    var s = new XMLSerializer();
+    return Array.prototype.map.call(node.childNodes, function (node) {
+        return s.serializeToString(node);
+    }).join('');
+};
+
+var getParseError = function (doc) {
+    // Firefox
+    if (doc.documentElement.tagName === 'parsererror' &&
+        doc.documentElement.namespaceURI === 'http://www.mozilla.org/newlayout/xml/parsererror.xml') {
+        return doc.documentElement;
+    }
+
+    // Chrome, Safari
+    if ((doc.documentElement.tagName === 'xml' || doc.documentElement.tagName === 'html') &&
+        doc.documentElement.childNodes &&
+        doc.documentElement.childNodes.length > 0 &&
+        doc.documentElement.childNodes[0].nodeName === 'parsererror') {
+        return doc.documentElement.childNodes[0];
+    }
+
+    // PhantomJS
+    if (doc.documentElement.tagName === 'html' &&
+        doc.documentElement.childNodes &&
+        doc.documentElement.childNodes.length > 0 &&
+        doc.documentElement.childNodes[0].nodeName === 'body' &&
+        doc.documentElement.childNodes[0].childNodes &&
+        doc.documentElement.childNodes[0].childNodes.length &&
+        doc.documentElement.childNodes[0].childNodes[0].nodeName === 'parsererror') {
+        return doc.documentElement.childNodes[0].childNodes[0];
+    }
+
+    return undefined;
+};
+
+var errorMessagePatterns = [
+    // Chrome, Safari, PhantomJS
+    new RegExp('^<h3[^>]*>This page contains the following errors:<\/h3><div[^>]*>(.+?)\n?<\/div>'),
+    // Firefox
+    new RegExp('^(.+)\n')
+];
+
+var extractParseError = function (errorNode) {
+    var content = innerXML(errorNode);
+    var i, match;
+
+    for(i = 0; i < errorMessagePatterns.length; i++) {
+        match = errorMessagePatterns[i].exec(content);
+
+        if (match) {
+            return match[1];
+        }
+    }
+    return undefined;
+};
+
+var failOnParseError = function (doc) {
+    var errorMessage;
+
+    if (doc === null) {
+        throw new Error('Parse error');
+    }
+
+    var parseError = getParseError(doc);
+    if (parseError !== undefined) {
+        errorMessage = extractParseError(parseError) || 'Parse error';
+        throw new Error(errorMessage);
+    }
+};
+
+exports.failOnParseError = function (doc) {
+    failOnParseError(doc);
+
+    return doc;
+};
+
+},{}]},{},[1])(1)
+});
